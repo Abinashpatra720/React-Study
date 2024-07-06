121a242076faa8567e39406ddcf0366fcb796eaa
Abinash Patra <apatra.1.618@gmail.com>
2024-07-06 11:55:21 +0530

    refractored

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
-
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me"),
-//     React.createElement(Pet, {
-//       animal: "Dog",
-//       name: "Luna",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       animal: "Bird",
-//       name: "Pepper",
-//       breed: "Cocket",
-//     }),
-//   ]);
-// };
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt me!</h1>
-      <Pet name ="Luna"animal= "Dog" breed = "Havanese"></Pet>
-      <Pet name ="Pepper"animal= "Bird" breed = "Cocket"></Pet>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
     </div>
   );
 };
+
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+const root = createRoot(container);
+root.render(<App />);
